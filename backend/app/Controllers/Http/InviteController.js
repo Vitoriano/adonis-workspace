488a0dd14ed64c5d56d4d88a67351f74eae5f5a2
Vitoriano <vitorianoernandes2012@gmail.com>
2020-07-20 20:28:53 -0300

    :sparkles: Implemrntaçao do modulo de convites
    Criando o modulo de convites e hook para envio de e-mail

--- backend/app/Controllers/Http/InviteController.js
+++ backend/app/Controllers/Http/InviteController.js
@@ -1,7 +1,17 @@
 
+const Invite = use('App/Models/Invite')
+
 class InviteController {
-  async store ({ request, response }) {
-    console.log(request.team)
+  async store ({ request, response, auth }) {
+    const invites = request.input('invites')
+
+    const data = invites.map(email => ({
+      email,
+      user_id: auth.user.id,
+      team_id: request.team.id
+    }))
+
+    await Invite.createMany(data)
   }
 }
 
