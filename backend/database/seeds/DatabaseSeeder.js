b86749a63a53b2e4a53283764868461162301d74
Vitoriano <vitorianoernandes2012@gmail.com>
2020-07-22 15:13:19 -0300

    :sparkles: Configurando o adonis ACL
    Configurando e sobrescrevendo os metodos default do adonis acl.

--- backend/database/seeds/DatabaseSeeder.js
+++ backend/database/seeds/DatabaseSeeder.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const User = use('App/Models/User')
+const Role = use('Adonis/Acl/Role')
+const Permission = use('Adonis/Acl/Permission')
 
 class DatabaseSeeder {
   async run () {
@@ -10,10 +12,44 @@ class DatabaseSeeder {
       password: '12345'
     })
 
-    await user.teams().create({
+    const createInvite = await Permission.create({
+      slug: 'invites_create',
+      name: 'Convidar membros'
+    })
+
+    const createProject = await Permission.create({
+      slug: 'projects_create',
+      name: 'Criar projetos'
+    })
+
+    const admin = await Role.create({
+      slug: 'adminstador',
+      name: 'Administrador'
+    })
+
+    const moderador = await Role.create({
+      slug: 'moderador',
+      name: 'Moderador'
+    })
+
+    await Role.create({
+      slug: 'visitante',
+      name: 'Visitante'
+    })
+
+    await admin.permissions().attach([createInvite.id, createProject.id])
+    await moderador.permissions().attach([createProject.id])
+
+    const team = await user.teams().create({
       name: 'Dev Na Rotina',
       user_id: user.id
     })
+
+    const teamJoin = await user.teamJoins()
+      .where('team_id', team.id)
+      .first()
+
+    await teamJoin.roles().attach([admin.id])
   }
 }
 
