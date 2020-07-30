9de9646e8f412331ba1988fe9b7b1aeadfda2732
Vitoriano <vitorianoernandes2012@gmail.com>
2020-07-30 14:56:32 -0300

    :sparkles: Layout da pagina de login

--- fontend/src/pages/Auth/SingIn/index.js
+++ fontend/src/pages/Auth/SingIn/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container, SignForm } from '../styles';
+import Button from '~/styles/components/Button';
 
-function SingIn() {
-  return <div />;
-}
+const SingIn = () => {
+  <Container>
+    <SignForm onSubmit={() => { } }>
+      <h1>Boas vindas</h1>
+
+      <span>E-MAIL</span>
+      <input type="email" name="email" />
+
+      <span>SENHA</span>
+      <input type="password" name="password" />
+
+      <Button size="big" type="submit">Entrar</Button>
+    </SignForm>
+  </Container>
+};
 
 export default SingIn;
